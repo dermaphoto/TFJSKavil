47e65ac0287a3fa4aa40435fa986e81f884a4c0d
amurto <amurtobasu@gmail.com>
2020-04-07 23:10:36 +0530

    updated

---
+++ precache-manifest.fefdf853ac5d10811a32dfc34e0a52bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f51f6d8228ba8bdd0605bed947f5c9e",
+    "url": "/TFJSKavil/index.html"
+  },
+  {
+    "revision": "6796ca7b91d51414ecdb",
+    "url": "/TFJSKavil/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e06ed147b399e634378b",
+    "url": "/TFJSKavil/static/js/2.f2967070.chunk.js"
+  },
+  {
+    "revision": "e46609aec777f9260d21834116f9e6de",
+    "url": "/TFJSKavil/static/js/2.f2967070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6796ca7b91d51414ecdb",
+    "url": "/TFJSKavil/static/js/main.09212199.chunk.js"
+  },
+  {
+    "revision": "57d6ca8c365ad2acb91a",
+    "url": "/TFJSKavil/static/js/runtime-main.c65b2840.js"
+  }
+]);
