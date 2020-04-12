59bb6167d575780f1655f5ab1aa46825ccc2dc28
thehardwareguy2000 <54811767+thehardwareguy2000@users.noreply.github.com>
2020-04-12 17:42:56 +0530

    done

---
+++ precache-manifest.f67ce4c27d44408922169bf03b447a3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b75ba68faeba3e05497f8218f5893dc",
+    "url": "/main_app/index.html"
+  },
+  {
+    "revision": "a52c848e4ffd959b392d",
+    "url": "/main_app/static/css/2.c9d9291a.chunk.css"
+  },
+  {
+    "revision": "15731a3c097b01ae1190",
+    "url": "/main_app/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "a52c848e4ffd959b392d",
+    "url": "/main_app/static/js/2.4d4236bb.chunk.js"
+  },
+  {
+    "revision": "46e9ddfdde897a51e5e3c081a17bf2ad",
+    "url": "/main_app/static/js/2.4d4236bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15731a3c097b01ae1190",
+    "url": "/main_app/static/js/main.a396c5d2.chunk.js"
+  },
+  {
+    "revision": "3dd622d2cd4b40a52b75",
+    "url": "/main_app/static/js/runtime-main.58f3dc30.js"
+  },
+  {
+    "revision": "6434d72ce9e16251c4f41f4e5a146567",
+    "url": "/main_app/static/media/female.6434d72c.png"
+  },
+  {
+    "revision": "9210da3d00166247e448b95269f5f1ab",
+    "url": "/main_app/static/media/male.9210da3d.png"
+  }
+]);
